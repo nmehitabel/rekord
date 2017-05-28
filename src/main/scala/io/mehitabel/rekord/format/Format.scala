b767bee49849517b10eca4df8961bc87ecdf78b8
Gavin Casey <gavin.casey@ontrackretail.co.uk>
2017-05-28 21:32:16 +0100

    add contramap

--- src/main/scala/io/mehitabel/rekord/format/Format.scala
+++ src/main/scala/io/mehitabel/rekord/format/Format.scala
@@ -2,6 +2,14 @@ package io.mehitabel.rekord
 package format
 import cats.data.{NonEmptyList => NEL, _}
 
-trait Format[T] {
-  def format(f: T): Validated[NEL[String], Output[String]]
+trait Formatted[T] {
+  def format(value: T): Validated[NEL[String], Output[String]]
+
+  def contramap[S](f: S => T): Formatted[S] = {
+    val self = this
+    new Formatted[S] {
+      def format(value: S): Validated[NEL[String], Output[String]] =
+        self.format(f(value))
+    }
+  }
 }
